38cd8d6541ab9e1d5036aa7acc102597eef33dab
LihleAvuNgcwembe <ngcwembelihle@gmail.com>
2025-01-27 12:07:19 +0200

    Added Database Authentication

--- src/main/java/net/javaguides/todo/config/SpringSecurityConfig.java
+++ src/main/java/net/javaguides/todo/config/SpringSecurityConfig.java
@@ -1,9 +1,12 @@
 package net.javaguides.todo.config;
 
+import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -16,7 +19,10 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableMethodSecurity
+@AllArgsConstructor
 public class SpringSecurityConfig {
+
+    private UserDetailsService userDetailsService;
 
     @Bean
     public static PasswordEncoder passwordEncoder(){
@@ -28,26 +34,19 @@ public class SpringSecurityConfig {
 
         http.csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests((authorize) -> {
-
-                    // All POST request must be authorised by an ADMIN
-                    authorize.requestMatchers(HttpMethod.POST, "/api/**").hasRole("ADMIN");
-
-                    // All PUT request must be authorised by an ADMIN
-                    authorize.requestMatchers(HttpMethod.PUT,"/api/**").hasRole("ADMIN");
-
-                    // All DELETE request must be authorised by an ADMIN
-                    authorize.requestMatchers(HttpMethod.DELETE,"/api/**").hasRole("ADMIN");
-
-                    // All GET request must be authorized by an ADMIN or USER
-                    authorize.requestMatchers(HttpMethod.GET,"/api/**").hasAnyRole("ADMIN","USER");
-
-                    // All PATCH request must be authorized by an ADMIN or USER
-                    authorize.requestMatchers(HttpMethod.PATCH,"/api/**").hasAnyRole("ADMIN","USER");
-
-                    // All GET request all public
-                    //authorize.requestMatchers(HttpMethod.GET,"/api/**").permitAll();
-
-                    // All request must be authenticated
+//                    // All POST request must be authorised by an ADMIN
+//                    authorize.requestMatchers(HttpMethod.POST, "/api/**").hasRole("ADMIN");
+//                    // All PUT request must be authorised by an ADMIN
+//                    authorize.requestMatchers(HttpMethod.PUT,"/api/**").hasRole("ADMIN");
+//                    // All DELETE request must be authorised by an ADMIN
+//                    authorize.requestMatchers(HttpMethod.DELETE,"/api/**").hasRole("ADMIN");
+//                    // All GET request must be authorized by an ADMIN or USER
+//                    authorize.requestMatchers(HttpMethod.GET,"/api/**").hasAnyRole("ADMIN","USER");
+//                    // All PATCH request must be authorized by an ADMIN or USER
+//                    authorize.requestMatchers(HttpMethod.PATCH,"/api/**").hasAnyRole("ADMIN","USER");
+//                    // All GET request all public
+//                    //authorize.requestMatchers(HttpMethod.GET,"/api/**").permitAll();
+//                    // All request must be authenticated
                     authorize.anyRequest().authenticated();
 
                 }).httpBasic(Customizer.withDefaults());
@@ -55,21 +54,26 @@ public class SpringSecurityConfig {
     }
 
     @Bean
-    public UserDetailsService userDetailsService(){
-
-        UserDetails lihle = User.builder()
-                .username("lihle")
-                .password(passwordEncoder().encode("password"))
-                .roles("USER")
-                .build();
-
-        UserDetails admin = User.builder()
-                .username("admin")
-                .password(passwordEncoder().encode("admin"))
-                .roles("ADMIN")
-                .build();
-
-        return new InMemoryUserDetailsManager(lihle, admin);
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
     }
+
+//    @Bean
+//    public UserDetailsService userDetailsService(){
+//
+//        UserDetails lihle = User.builder()
+//                .username("lihle")
+//                .password(passwordEncoder().encode("password"))
+//                .roles("USER")
+//                .build();
+//
+//        UserDetails admin = User.builder()
+//                .username("admin")
+//                .password(passwordEncoder().encode("admin"))
+//                .roles("ADMIN")
+//                .build();
+//
+//        return new InMemoryUserDetailsManager(lihle, admin);
+//    }
 
 }
